b780ec1f652bedf8d18dfbde3ba6be376c06e942
Andrew Wilkinson <andrewscm2020@gmail.com>
2020-12-02 04:30:47 -0800

    Pushing up most recent work

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 // Create html elements for our quiz
 // Grab html elements and bring to the javascript
-
+var btnholder = document.getElementById("buttonwrapper");
 var button1 = document.getElementById("answer1");
 var button2 = document.getElementById("answer2");
 var button3 = document.getElementById("answer3");
@@ -10,19 +10,56 @@ var button4 = document.getElementById("answer4");
 var questionboxtext = document.querySelector(".questiontext");
 var questioncontainer = document.querySelector(".questioncontainer");
 
+var startbutt = document.getElementById("startbutton2");
+
+//These are used for grabbing the correct inputs for local storage saving. CountdownEl is also used for the timer mechanic, and not just for the player input (uncontrolled variable by the player).
+var playerinput = document.getElementById("initials");
+var countdownEl = document.getElementById('Timeleft');
+
+
+//It starts as an empty array until user actions start to fill them. 
+
+var intials = [];
+var score = [];
+
+//These are used for the correct outputs/endpoints which will have local storage data distributed to the front end of the website.
+var playerrecord = document.getElementById("playerrecord");
+var finaltime = document.getElementById("finaltime");
+
+//This is to combine the previous 6 variables, to say that the variables for local storage should equal the text of the input of the player and whatever time is in the timeleft element.
+initials.textContent = playerinput;
+score.textContent = countdownEl;
+
+
 
 
 var startingMinutes =  5;
 let time = startingMinutes * 60;
 
-var countdownEl = document.getElementById('Timeleft');
+
 
 
 function gameover () {
+
+
+    // for loop for setting initials to be pushed to DOM and appending LI (I DO NOT BELIEVE MY LINE 46-52 WORKS)
+    localStorage.setItem("Timeleft", time);
+    for (var score = 0; i < score.length; score++) {
+        var scorelistItem = document.createElement("li");
+
+        scorelistItem.textContent = score[i];
+
+        countdownEl.appendChild(scorelistItem);
+    }
+
+
     document.querySelector('.questioncontainer').style.display = "none";
     document.getElementById("myh2").style.display = "none";
     startbutt.style.display = "block";
-}
+    
+
+    }
+
 
 
 setInterval(updateTimeleft, 1000);
@@ -42,55 +79,7 @@ function updateTimeleft() {
         gameover();
     }
     displaymyTime();
-    // var minutes = Math.floor(time / 60);
-    // var seconds = time % 60;
 
-    // seconds = seconds < 10 ? "0" + seconds: seconds;
-    // countdownEl.innerHTML = `${minutes}: ${seconds}`;
-}
-// funtion displayTime () {
-// //     var minutes = Math.floor(time / 60);
-// //     var seconds = time % 60;
-
-// //     seconds = seconds < 10 ? "0" + seconds: seconds;
-// //     countdownEl.innerHTML = `${minutes}: ${seconds}`;
-//  }
-
-
-
-
-
-
-
-
-
-
-
-// function updateTimeleft() {
-//     var minutes = Math.floor(time / 60);
-//     var seconds = time % 60;
-
-//     time--;
-
-//     seconds = seconds < 5 ? "0" + seconds: seconds;
-//     countdownEl.innerHTML = `${minutes}: ${seconds}`;
-
-
-// }
-
-
-
-
-
-
-// for the last part where time has run out, and the user fails to win
-// var Timer = document.getElementById("TimeGone").setTimeout(function, 1000);
-
-
-// Update html elements with answers the user outputs
-
-// Make an array of objects where in each object has the question text, and the answer for any answer I want to put in the page.
-// Setup dummy info don't make question content yet
 var i = 0;
 var questionarray =  [
     {
@@ -139,20 +128,22 @@ function generatequestion ()
 
 
 
-var startbutt = document.getElementById("startbutton2");
-
-
-
-// var initialid = document.getElementById("initialid");
-// var scoreid = document.getElementById("scoreid");
-
-// initialid.append("")
-// scoreid.append("")
 
 document.querySelector('.questioncontainer').style.display = "none";
 document.getElementById('myh2').style.display = "none";
 
 startbutt.addEventListener('click',event => {
+
+    // for loop for setting initials to be pushed to DOM and appending LI ((I DO NOT BELIEVE MY LINEs 138-145 WORKS))
+    localStorage.setItem("initials", initials);
+    for (var initials = 0; i < initials.length; initials++) {
+        var initialslistItem = document.createElement("li");
+
+        initialslistItem.textContent = intials[i];
+
+        playerinput.appendChild(initialslistItem);
+    }
+
     document.querySelector('.questioncontainer').style.display = "";
     startbutt.style.display = "none";
     document.getElementById('myh2').style.display = "";
@@ -168,7 +159,7 @@ startbutt.addEventListener('click',event => {
 
 
 
-var btnholder = document.getElementById("buttonwrapper");
+
 
 
 
